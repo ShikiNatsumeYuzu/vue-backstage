a6436e3862cc3d6929cb061e2e51bdac6dd0c8b6
Natsu <983213139@qq.com>
2020-01-21 01:01:55 +0800

    2020-1-21 01:01:52

--- src/components/users.vue
+++ src/components/users.vue
@@ -29,9 +29,9 @@
         <el-table-column label="状态" v-slot="state">
           <el-switch v-model="state.row.mg_state" @change="userStateChanged(state.row)" />
         </el-table-column>
-        <el-table-column label="操作" width="200px">
+        <el-table-column label="操作" width="200px" v-slot="slot">
           <!-- 修改按钮 -->
-          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(slot.row.id)"></el-button>
           <!-- 删除按钮 -->
           <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
           <!-- 分配角色 -->
@@ -67,6 +67,24 @@
         <el-button @click="addDialogVisible = false">取 消</el-button>
       </span>
     </el-dialog>
+    <!-- 修改用户的对话框 -->
+    <el-dialog title="修改用户" :visible.sync="editDialogVisible" width="50%" :close-on-click-modal="false" @close="editDialogClosed">
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
+        <el-form-item label="用户名">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="editForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="editUserInfo">确 定</el-button>
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -80,6 +98,7 @@ export default {
       },
       userlist: [],
       total: 0,
+      // 控制添加用户对话框的显示与隐藏
       addDialogVisible: false,
       // 添加用户的表单数据对象
       addForm: {
@@ -126,6 +145,29 @@ export default {
           validator: this.checkMobile,
           trigger: 'blur'
         }]
+      },
+      // 控制修改用户对话框的显示与隐藏
+      editDialogVisible: false,
+      // 查询到的用户信息对象
+      editForm: {},
+      // 修改表单的验证规则对象
+      editFormRules: {
+        email: [{
+          required: true,
+          message: '请输入邮箱',
+          trigger: 'blur'
+        }, {
+          validator: this.checkEmail,
+          trigger: 'blur'
+        }],
+        mobile: [{
+          required: true,
+          message: '请输入手机号',
+          trigger: 'blur'
+        }, {
+          validator: this.checkMobile,
+          trigger: 'blur'
+        }]
       }
     }
   },
@@ -133,15 +175,37 @@ export default {
     this.getUserList()
   },
   methods: {
+    // 修改用户信息并提交
+    editUserInfo() {
+      this.$refs.editFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$axios.put(`/users/${this.editForm.id}`, {
+          email: this.editForm.email,
+          mobile: this.editForm.mobile
+        })
+        if (res.meta.status == 200) {
+          this.getUserList()
+          this.$message.success("更新用户信息成功！")
+          this.editDialogVisible = false
+        } else {
+          this.$message.error("更新用户信息失败！")
+        }
+      })
+    },
+    // 监听修改用户对话框的关闭事件
+    editDialogClosed() {
+      this.editForm = {}
+      this.$refs.editFormRef.resetFields()
+    },
     // 点击确定添加用户
     addUser() {
       this.$refs.addFormRef.validate(async valid => {
         if (!valid) return
         const { data: res } = await this.$axios.post("/users", this.addForm)
         if (res.meta.status == 201) {
+          this.getUserList()
           this.$message.success("添加用户成功！")
           this.addDialogVisible = false
-          this.getUserList()
         } else {
           this.$message.error("添加用户失败！")
         }
@@ -179,10 +243,12 @@ export default {
       this.queryInfo.pagenum = newPage
       this.getUserList()
     },
+    // 搜索用户
     seach() {
       this.queryInfo.pagenum = 1
       this.getUserList()
     },
+    // 修改用户状态
     async userStateChanged(userInfo) {
       const { data: res } = await this.$axios.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
       if (res.meta.status == 200) {
@@ -192,6 +258,7 @@ export default {
         this.$message.error("更新用户状态失败！")
       }
     },
+    // 查询用户列表
     async getUserList() {
       const { data: res } = await this.$axios.get('/users', { params: this.queryInfo })
       if (res.meta.status == 200) {
@@ -200,6 +267,16 @@ export default {
       } else {
         this.$message.error("获取用户列表失败！")
       }
+    },
+    // 展示编辑用户的对话框
+    async showEditDialog(id) {
+      const { data: res } = await this.$axios.get(`/users/${id}`)
+      if (res.meta.status == 200) {
+        this.editForm = res.data
+      } else {
+        return this.$message.error("查询用户信息失败！")
+      }
+      this.editDialogVisible = true
     }
   }
 }
